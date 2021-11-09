9b4d42b5bb9f4d6b5b404b3653f40db36a4bf0af
phil <phil.roth077@gmail.com>
2021-11-09 11:03:28 -0800

    fix apppp

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
   resources :posts
   resources :users, only: [:index]
-  
+
   get '/me', to: "auth#me"
   post '/login', to: "auth#login"
   post '/signup', to: "auth#signup"
   delete '/logout', to: "auth#logout"
+    
+  get "/hello", to: "application#hello_world"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
